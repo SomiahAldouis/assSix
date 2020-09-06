b07700f20eb73a939ef4f8f2b51a97ecadb407f5
somiahgit <somiahjamal1997@gmail.com>
2020-09-06 07:29:11 +0300

    second commit

--- src/main.kt
+++ src/main.kt
@@ -1,6 +1,17 @@
 fun main(){
 
 }
+interface UserInteractions{
+    fun click()
+    fun drag()
+
+}
+
+interface ClickType: UserInteractions{
+    fun longClick()
+    fun rightClick()
+    fun doubleClick()
+}
 
 open class Employee(name:String,age:Int,salary:Double){
     var name=""
@@ -14,8 +25,27 @@ class WebDeveloper(webDevName: String, webDevSalary: Double, webDevAge: Int)
 }
 
 class AndroidDeveloper(webDevName: String, webDevSalary: Double, webDevAge: Int)
-                    :Employee(webDevName,webDevAge,webDevSalary){
+                    :Employee(webDevName,webDevAge,webDevSalary),ClickType{
     fun android(){}
+    override fun longClick() {
+
+    }
+
+    override fun rightClick() {
+
+    }
+
+    override fun doubleClick() {
+
+    }
+
+    override fun click() {
+
+    }
+
+    override fun drag() {
+
+    }
 }
 
 class IosDeveloper(webDevName: String, webDevSalary: Double, webDevAge: Int)
